d28709044c03e8c9f91ff0b4c871a5339b50d983
Imon <contact@imonweb.co.uk>
2023-04-11 21:28:34 +0100

    proj1

--- proj1/slider.js
+++ proj1/slider.js
@@ -1,7 +1,9 @@
-// const left = document.querySelector(".left")
-// const right = document.querySelector(".right")
-// const slider = document.querySelector(".slider")
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+ 
+let slideNumber = 1;
+ 
 
-// right.addEventListener("click", () => {
-//   slider.style.transform = `translateX(-800px)`
-// })
+
+ 
